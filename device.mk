8b6f64f26ab3836498635953d32575fa0adf71a6
johnmart19 <johnivan19999@gmail.com>
2024-01-18 22:03:20 +0200

    alioth: Patch Dolby Libs to use fallback to VNDK33 stagefright_foundation
    - VNDK34 part was updated and our service requires to have fallback in order to work appropriately
    - Copy Respectfully arm and arm64 libs
    
    Change-Id: I21912f3cbee49fbd3dadd364a847dde54e53a4c3

--- device.mk
+++ device.mk
@@ -36,6 +36,11 @@ TARGET_USES_MIUI_DOLBY := true
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/dolby/config/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
+# Dolby VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm64/arch-arm-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33-dolby.so \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33-dolby.so
+
 # Kernel
 TARGET_KERNEL_DIR ?= device/xiaomi/alioth-kernel
 LOCAL_KERNEL := $(TARGET_KERNEL_DIR)/Image
